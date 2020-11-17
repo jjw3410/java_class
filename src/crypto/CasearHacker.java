8fdd3f019bb6e37ab281eb68bcec148baffb2f61
J W Jeong <jjw3410@naver.com>
2020-11-17 15:29:42 +0900

    change commit

---
+++ src/crypto/CasearHacker.java
@@ -0,0 +1,11 @@
+package crypto;
+
+public class CasearHacker {
+	
+	//암호문과 charset과 알고리즘이 시저암호 방식임을 알고 있다고 가정한 상태로
+	//암호문을 이용하여 평문을 추출해보세요.
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ src/crypto/Cipher.java
@@ -0,0 +1,17 @@
+package crypto;
+
+//추상클래스: 클래스 내부에 추상 메서드를 지닌 클래스
+public abstract class Cipher {
+
+	
+	
+	abstract public String encryption(String plain);
+		
+	
+	
+	abstract public String decryption(String crypto);
+	
+	
+}
+
+
